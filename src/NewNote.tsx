307ca97a5afe0d9e9bf37a0e951eedc4b1037e11
Liviu <liviutoma946@gmail.com>
2023-06-07 00:32:26 +0300

    passed onsubmit functions to note module

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,13 +1,17 @@
+import { NoteData, Tag } from './App'
 import NoteForm from './NoteForm'
 
-function NewNote() {
-  const onSubmit = () => {
-    
-  }
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
     <>
         <h1 className='mb-4'>New Note</h1>
-        <NoteForm onSubmit={onSubmit} />
+        <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
     </>
   )
 }
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 import CreatableReactSelect from 'react-select/creatable';
 import { FormEvent, useRef, useState } from 'react';
 import { NoteData, Tag } from './App';
+import { v4 as uuidV4 } from 'uuid';
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
 }
 
-function NoteForm({ onSubmit }: NoteFormProps) {
+function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
@@ -37,6 +40,11 @@ function NoteForm({ onSubmit }: NoteFormProps) {
                 <Form.Group controlId='tags'>
                   <Form.Label>Tags</Form.Label>
                   <CreatableReactSelect 
+                    onCreateOption={label => {
+                      const newTag = { id: uuidV4(), label}
+                      onAddTag(newTag)
+                      setSelectedTags(prev => [...prev, newTag])
+                    }}
                     value={selectedTags.map(tag => {
                       console.log(tag);
                       return { label: tag.label, value: tag.id }
